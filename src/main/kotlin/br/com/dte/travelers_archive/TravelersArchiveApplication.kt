eeeac928fc7fb6b2dd13b7338fe4dca572cbf3b2
Madu <madu3811@gmail.com>
2026-03-20 20:28:08 -0300

    (build) inicia projeto

---
+++ src/main/kotlin/br/com/dte/travelers_archive/TravelersArchiveApplication.kt
@@ -0,0 +1,11 @@
+package br.com.dte.travelers_archive
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TravelersArchiveApplication
+
+fun main(args: Array<String>) {
+	runApplication<TravelersArchiveApplication>(*args)
+}
---
+++ src/test/kotlin/br/com/dte/travelers_archive/TravelersArchiveApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.dte.travelers_archive
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TravelersArchiveApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
